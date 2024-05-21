36e43d0f4c4d6b95f9d64cfdb6b7fa3914a9b356
Jiucheng Zang <zangjiucheng@gmail.com>
2024-05-21 00:31:57 -0400

    Update App Icon (May 21th 2024)

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 
 from src.ui import MyApp
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('data/icon.ico'))
     window = MyApp()
     window.show()
     # func = Func(window)
